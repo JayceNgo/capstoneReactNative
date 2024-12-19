89017ba988244f11068276f576d9b5dbef2c95d8
JayceNgo <60013030+JayceNgo@users.noreply.github.com>
2024-12-19 00:28:53 -0500

    Set Up OnBoading

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,161 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Text, Platform,View, TextInput, ScrollView,TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { validateEmail, validateName } from '../../utils/validation.js'
+import colors from '../../constants/Colors';
+import {fonts} from '../../constants/fonts';
+import { useAuth, AuthProvider } from '../../contexts/AuthContext';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
+    <AuthProvider>
+      <Onboarding />
+    </AuthProvider>
+  );
+}
+
+const Onboarding = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const { authActions } = useAuth();
+
+
+  const handleNameChange = (text:any) => {
+    setName(text);
+    setIsValid(validateName(text) && validateEmail(email));
+  };
+
+  const handleEmailChange = (text:any) => {
+    setEmail(text);
+    setIsValid(validateName(name) && validateEmail(text));
+  };
+
+  const handlePress = async () => {
+    // call onboard function from AUthContext with name and email
+    await authActions.onboard({ name, email });
+  };
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        source={require('@/assets/images/Logo.png')}
+        style={styles.logo}
+        accessible={true}
+        accessibilityLabel={"Little Lemon Logo"}/>
+        <Text style={styles.welcomeText}>
+              Welcome to Little Lemon Restaurant !
+          </Text>
+          <Image
+        source={require('@/assets/images/restaurant.jpg')}
+        style={styles.restaurantImage}
+        accessible={true}
+        accessibilityLabel={"Restaurant Logo"}/>
+        <View style={styles.inputContainer}>
+            <Text style={styles.inputTitle}>Full Name</Text>
+            <TextInput
+                placeholder="Enter your name"
+                value={name}
+                onChangeText={handleNameChange}
+                style={styles.input}
+            />
+             <Text style={styles.inputTitle}>Email Address</Text>
+            <TextInput
+                value={email}
+                onChangeText={handleEmailChange}
+                placeholder="Enter your email"
+                keyboardType="email-address"
+                autoCapitalize='none'
+                style={styles.input}
+            />
+            </View>
+            
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                onPress={handlePress}
+                disabled={!isValid}
+                style={isValid ? styles.enabled : styles.disabled}
+            >
+                <Text style={isValid ? styles.textEnabled : styles.textDisabled}>Next</Text>
+            </TouchableOpacity >
+          </View>
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    gap: 8,
+    padding: 20,
+    paddingTop: 20,
+    backgroundColor: colors.background,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    width: "70%",
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  restaurantImage: {
+    width: "90%",
+    height: 150,
+    borderRadius: 20,
+    marginBottom: 36,
+  },
+  welcomeText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 25,
+    fontFamily: fonts.karla.extraBoldItalic,
+    color: colors.secondaryOrange,
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  inputTitle: {
+    fontSize: 20,
+    fontFamily: fonts.karla.extraBoldItalic,
+    color: colors.text,
+  },
+  input: {
+    fontSize: 16,
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    marginBottom: 20,
+    borderColor: colors.secondaryOrange,
+  },
+  buttonContainer: {
+    width: '100%',
+    marginTop: 20,
+  },
+  enabled: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: colors.primaryGreen,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  disabled: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: colors.background,
+    borderColor: colors.primaryGreen,
+    borderWidth: 1.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textEnabled: {
+    fontSize: 18,
+    fontFamily: fonts.karla.bold,
+    color: colors.background,
+  },
+  textDisabled: {
+    fontSize: 18,
+    fontFamily: fonts.karla.bold,
+    color: colors.text, 
   },
 });
